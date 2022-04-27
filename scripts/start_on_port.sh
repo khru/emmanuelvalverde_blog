3beeaac6988df47d8e3b4d5e41460d0cd326414c
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-04-27 16:25:46 +0200

    remove unused script

--- scripts/start_on_port.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# It doesn't matter to use the front or de back container we just need a container with NPM and node install
-PORT=$(grep -w NODE_PORT .env | cut -d '=' -f2);
-HOST=$(grep -w NODE_HOSTNAME .env | cut -d '=' -f2);
-
-next start -H ${HOST} -p ${PORT} $@
-
-return 0;
