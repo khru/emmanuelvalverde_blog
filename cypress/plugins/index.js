25538c99dfeaf736079bd5aca2128b6543ae07ef
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-12-29 15:50:05 +0100

    chore: update dependencies, fix cypress update and update to next 13

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,6 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
+const browserify = require("@cypress/browserify-preprocessor");
+
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -20,5 +22,9 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('file:preprocessor', cucumber())
+  const options = {
+    ...browserify.defaultOptions,
+    typescript: require.resolve("typescript"),
+  };
+  on('file:preprocessor', cucumber(options))
 };
